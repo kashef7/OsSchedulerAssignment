b57695587b6e60845a36fb5da08ecbaa7811dc93
kashef7 <11410120230477@stud.cu.edu.eg>
2025-12-17 23:29:01 +0200

    Updated Process class to be same as lab

--- src/Process.java
+++ src/Process.java
@@ -1,6 +1,44 @@
-public class Process {
-    String name;
-    int arrivalTime;
-    int burstTime;
-    int priority;
+//The class of the process
+class Process {
+    private int arrivalTime;
+    private int burstTime;
+    private int waitingTime;
+    private String name;
+
+    public Process(int arriveTime, int burstTime, String name) {
+        this.arrivalTime = arriveTime;
+        this.burstTime = burstTime;
+        this.name = name;
+        waitingTime = 0;
+    }
+
+    // Get the arrive time value
+    public int getArrivalTime() {
+        return arrivalTime;
+    }
+
+    // Set the arrive time value
+    public void setArrivalTime(int arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    // Get the burst time value
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public int getWaitingTime() {
+        return waitingTime;
+    }
+
+    // Set the burst time value
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    // Get the process name
+    public String getName() {
+
+        return name;
+    }
 }
