65d8b4d02253c00e089395bb7e361171137764d0
yousefashraf35 <youash335@gmail.com>
2025-12-18 18:14:44 +0200

    Round Robin (done)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-public class Main{
+public class Main {
     public static void main(String[] args) {
         PreemptiveSJF preemptiveSJF = new PreemptiveSJF();
-        preemptiveSJF.schedule(2,0,1);
+        preemptiveSJF.schedule(2, 0, 1);
     }
 }
