5ae7bb79ab0f4e504b0fc6d7a0ba1b955d2acd3c
Ahmed Atef <20230026@stud.fci-cu.edu.eg>
2025-12-20 23:26:40 +0200

    Implement printResults method in RoundRobin for testrunner
    Added printResults method to display execution order and process results.

--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -65,6 +65,7 @@ public class RoundRobin extends Scheduler {
 
             lastProcess = currentProcess;
         }
+        printResults();
     }
 
     void calculateAvgOfWaitingAndTurnAroundTime() {
@@ -92,5 +93,59 @@ public class RoundRobin extends Scheduler {
             System.out.print(p.getName() + " ");
         }
         System.out.println();
+    }
+      void printResults() {
+        // populate inherited ExcutionOrder from the internal orderExecution queue
+        ExcutionOrder.clear();
+        for (Process p : orderExecution) {
+            ExcutionOrder.add(p.getName());
+        }
+
+        // build sorted copy of outputProcesses (by numeric suffix if present)
+        List<Process> sorted = new ArrayList<>(outputProcesses);
+        sorted.sort((a, b) -> {
+            String na = a.getName();
+            String nb = b.getName();
+            Integer ia = null, ib = null;
+            try { ia = Integer.valueOf(na.replaceAll("\\D+", "")); } catch (Exception e) {}
+            try { ib = Integer.valueOf(nb.replaceAll("\\D+", "")); } catch (Exception e) {}
+            if (ia != null && ib != null) return ia.compareTo(ib);
+            return na.compareTo(nb);
+        });
+
+        // Copy results back into original 'processes' so Main can read actual values
+        Map<String, Process> origByName = new HashMap<>();
+        for (Process p : processes) origByName.put(p.getName(), p);
+        for (Process p : sorted) {
+            Process orig = origByName.get(p.getName());
+            if (orig != null) {
+                orig.setWaitingTime(p.getWaitingTime());
+                orig.setTurnAroundTime(p.getTurnAroundTime());
+                orig.setRemainingTime(p.getRemainingTime());
+            }
+        }
+
+        // print header similar to AgScheduler
+        System.out.println("Gantt Chart: "); // RoundRobin doesn't track segments; left blank
+        System.out.println();
+        System.out.println("\"executionOrder\": " + ExcutionOrder + ",");
+
+        System.out.println("\"processResults\": [");
+        double totalWait = 0;
+        double totalTurn = 0;
+        for (int i = 0; i < sorted.size(); i++) {
+            Process p = sorted.get(i);
+            totalWait += p.getWaitingTime();
+            totalTurn += p.getTurnAroundTime();
+            System.out.print("  {\"name\": \"" + p.getName() + "\", \"waitingTime\": " + p.getWaitingTime() +
+                             ", \"turnaroundTime\": " + p.getTurnAroundTime() + "}");
+            if (i < sorted.size() - 1) System.out.println(",");
+            else System.out.println();
+        }
+        System.out.println("],");
+        double avgW = sorted.isEmpty() ? 0.0 : (totalWait / sorted.size());
+        double avgT = sorted.isEmpty() ? 0.0 : (totalTurn / sorted.size());
+        System.out.println("\"averageWaitingTime\": " + avgW + ",");
+        System.out.println("\"averageTurnaroundTime\": " + avgT);
     }
 }
